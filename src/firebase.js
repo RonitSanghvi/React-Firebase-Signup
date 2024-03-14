b28ca6ec4709dfedda1cba96ed90ef42bc5542aa
Admin <ronitsanghvi2000@gmail.com>
2024-03-14 13:54:26 -0400

    gitignore updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore"; // Import Firestore
-
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHoH1GtZ4q_YApfHpsA1U-Amks0DC-6dY",
-  authDomain: "react-firebase-auth-b7093.firebaseapp.com",
-  projectId: "react-firebase-auth-b7093",
-  storageBucket: "react-firebase-auth-b7093.appspot.com",
-  messagingSenderId: "190868097804",
-  appId: "1:190868097804:web:05d5b90271de87f2895729"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
